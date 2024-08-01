ae7f4637e998f2f1eebfd85105e4a4571f368612
Sanyam Bassi <148398605+sanyambassi@users.noreply.github.com>
2024-08-01 18:56:52 -0400

    Update launchWorkshop.sh

--- launchWorkshop.sh
+++ launchWorkshop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+echo
 # Prompt user for password and confirm
 while true; do
     read -sp "Create/Enter a new password for CipherTrust Manager's admin user: " PASSWORD
@@ -10,7 +10,7 @@ while true; do
     echo "Passwords do not match. Please try again."
 done
 echo "Passwords match."
-
+echo
 # Define common variables
 USER="admin"
 NO_SSL_VERIFY="--nosslverify"
@@ -18,88 +18,88 @@ STACK_NAME="aws-thales-crdp-workshop"
 TEMPLATE_FILE="$PWD/cloud_formation_template.yaml"
 REGION="us-east-1"
 K8_DEPLOYMENT_FILE="$PWD/k8-deployment.yaml" 
-
+echo
 # Create a key pair in AWS in us-east-1
 echo "Creating a keypair named ksadmin_cm in AWS us-east-1..."
 aws ec2 create-key-pair --key-name ksadmin_cm --query 'KeyMaterial' --region us-east-1 --output text > ksadmin_cm.pem
 echo "Created a keypair named ksadmin_cm in AWS us-east-1, and saved in the current working directory."
-
+echo
 # Create CloudFormation stack
 echo "Creating CloudFormation stack..."
 aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://$TEMPLATE_FILE --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM --region $REGION
-
+echo
 # Wait for stack to be created
 echo "Waiting for stack to be created..."
 aws cloudformation wait stack-create-complete --stack-name $STACK_NAME --region $REGION
 echo "CloudFormation stack created successfully."
-
+echo
 # Retrieve the public IP of the CipherTrust Manager
 echo "Retrieving public IP of the CipherTrust Manager..."
 PUBLIC_IP=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --region $REGION --query "Stacks[0].Outputs[?OutputKey=='PublicIPAddress'].OutputValue" --output text)
 echo "Public IP: $PUBLIC_IP"
-
+echo
 # Retrieve the private IP of the CipherTrust Manager
 echo "Retrieving private IP of the CipherTrust Manager..."
 PRIVATE_IP=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --region $REGION --query "Stacks[0].Outputs[?OutputKey=='InstancePrivateIp'].OutputValue" --output text)
 echo "Private IP: $PRIVATE_IP"
-
+echo
 # Update k8-deployment.yaml with the private IP
 echo "Updating k8 deployment manifest with the private IP..."
 sed -i "/name: KEY_MANAGER_HOST/{n;s/value:.*/value: \"$PRIVATE_IP\"/}" $K8_DEPLOYMENT_FILE
-
+echo
 # Check if the file has been updated
 echo "Verifying if the deployment manifest has been updated..."
 grep -A 1 "name: KEY_MANAGER_HOST" $K8_DEPLOYMENT_FILE
-
+echo
 # Define the URL with the retrieved public IP
 URL="https://$PUBLIC_IP"
 KSCTL_URL="$URL/downloads/ksctl_images.zip"
-
+echo
 # Retrieve ksctl binaries
 echo "Retrieving ksctl binaries..."
 wget $KSCTL_URL -O ksctl_images.zip --no-check-certificate
 unzip ksctl_images.zip
 chmod +x ksctl-linux-amd64
 echo "ksctl binaries retrieved and ready to use."
-
+echo
 # Change admin user's default password
 echo "Changing admin user's default password..."
 ./ksctl-linux-amd64 changepw -n $PASSWORD -c $PASSWORD --user $USER --password admin --url $URL $NO_SSL_VERIFY
 echo "Admin user's password changed successfully."
-
+echo
 # Get trial license ID
 echo "Retrieving trial license ID..."
 TRIAL_ID=$(./ksctl-linux-amd64 licensing trials list --url $URL --user $USER --nosslverify --password $PASSWORD | jq -r '.resources[0].id')
-
+echo
 # Activate the trial
 echo "Activating the trial license..."
 ./ksctl-linux-amd64 licensing trials activate --id $TRIAL_ID --url $URL --user $USER --nosslverify --password $PASSWORD
 echo "Trial license activated successfully."
-
+echo
 # Create character sets
 echo "Creating a new character set for All alphabets..."
 CHARACTER_SET_ID_1=$(./ksctl-linux-amd64 data-protection character-sets create --name "All Alphabets" --range 0041-005A,0061-007A --user $USER --password $PASSWORD $NO_SSL_VERIFY --url $URL | jq -r '.id')
-
+echo
 # List character sets and extract the  ID for All digits character set
 CHARACTER_SET_ID_2=$(./ksctl-linux-amd64 data-protection character-sets list --url $URL --user $USER $NO_SSL_VERIFY --password $PASSWORD | jq -r '.resources[] | select(.name == "All digits") | .id')
-
+echo
 # Create client profile and extract reg_token
 echo "Creating CRDP application on CipherTrust manager and extracting registration token..."
 output=$(./ksctl-linux-amd64 data-protection client-profiles create --app-connector-type CRDP --name CRDP_Demo_App --url $URL --user $USER --password "$PASSWORD" $NO_SSL_VERIFY --csr-parameters '{"csr_cn":"crdpviaksctl"}' --configurations '{"log_level":"INFO","heartbeat_interval":30}')
 reg_token=$(echo "$output" | jq -r '.reg_token')
 echo "CRDP application created and registration token retrieved."
-
+echo
 # Update Kubernetes config
 echo "Updating Kubernetes configuration..."
 CLUSTER_NAME=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --region $REGION --query "Stacks[0].Outputs[?OutputKey=='EKSClusterName'].OutputValue" --output text)
 aws eks update-kubeconfig --region $REGION --name $CLUSTER_NAME
 echo "Kubernetes configuration updated successfully."
-
+echo
 # Create Kubernetes secret with reg_token
 echo "Creating Kubernetes secret with registration token..."
 kubectl create secret generic regtoken --from-literal=reg_token="$reg_token"
 echo "Kubernetes secret created successfully."
-
+echo
 # Create user sets
 echo "Creating user sets..."
 USER_SET_ID_1=$(./ksctl-linux-amd64 data-protection user-sets create --name US_App_Generic_User --users app_generic_user --user $USER --password $PASSWORD $NO_SSL_VERIFY --url $URL | jq -r '.id')
@@ -107,12 +107,12 @@ USER_SET_ID_2=$(./ksctl-linux-amd64 data-protection user-sets create --name US_A
 USER_SET_ID_3=$(./ksctl-linux-amd64 data-protection user-sets create --name US_App_User_Last4 --users app_user_last4 --user $USER --password $PASSWORD $NO_SSL_VERIFY --url $URL | jq -r '.id')
 USER_SET_ID_4=$(./ksctl-linux-amd64 data-protection user-sets create --name US_App_User_First2_Last4 --users app_user_first2_last4 --user $USER --password $PASSWORD $NO_SSL_VERIFY --url $URL | jq -r '.id')
 echo "User sets created successfully."
-
+echo
 # List masking formats anxtract required IDs
 echo "Extracting masking format IDs..."
 MASKING_FORMAT_ID_1=$(./ksctl-linux-amd64 data-protection masking-formats list --url $URL --user $USER --password $PASSWORD $NO_SSL_VERIFY | jq -r '.resources[] | select(.name == "SHOW_LAST_FOUR") | .id')
 MASKING_FORMAT_ID_2=$(./ksctl-linux-amd64 data-protection masking-formats list --url $URL --user $USER --password $PASSWORD $NO_SSL_VERIFY | jq -r '.resources[] | select(.name == "SHOW_FIRST_TWO_LAST_FOUR") | .id')
-
+echo
 # Create user set policy JSON file
 echo "Creating user set policy JSON file..."
 cat <<EOF > user-set-policy.json
@@ -144,12 +144,12 @@ cat <<EOF > user-set-policy.json
 ]
 EOF
 echo "User set policy JSON file created successfully."
-
+echo
 # Create access policy
 echo "Creating access policy..."
 ACCESS_POLICY_ID=$(./ksctl-linux-amd64 data-protection access-policies create --name AP01 --default-error-replacement-value "Unauthorized" --default-reveal-type "Error Replacement Value" --jsonfile user-set-policy.json --user $USER --password $PASSWORD $NO_SSL_VERIFY --url $URL | jq -r '.id')
 echo "Access policy created successfully."
-
+echo
 # Create key JSON file
 echo "Creating key JSON file..."
 cat <<EOF > createkey.json
@@ -175,18 +175,18 @@ cat <<EOF > createkey.json
 }
 EOF
 echo "Key JSON file created successfully."
-
+echo
 # Create key
 echo "Creating key..."
 KEY_ID=$(./ksctl-linux-amd64 keys create -j createkey.json --url $URL --user $USER --password $PASSWORD $NO_SSL_VERIFY | jq -r '.id')
 echo "Key created successfully."
-
+echo
 # Create protection policies
 echo "Creating protection policies..."
 ./ksctl-linux-amd64 data-protection protection-policies create --url $URL --user $USER --password $PASSWORD $NO_SSL_VERIFY --algorithm "FPE/AES/UNICODE" --key "FPEKey" --access-policy-name AP01 --character-set-id "$CHARACTER_SET_ID_1" --disable-versioning --tweak 1234567812346578 --tweak-algorithm SHA256 --name PPol1
 ./ksctl-linux-amd64 data-protection protection-policies create --url $URL --user $USER --password $PASSWORD $NO_SSL_VERIFY --algorithm "FPE/AES/UNICODE" --key "FPEKey" --access-policy-name AP01 --character-set-id "$CHARACTER_SET_ID_2" --disable-versioning --tweak 1234567812346578 --tweak-algorithm SHA256 --name PPol2
 echo "Protection policies created successfully."
-
+echo
 # Create K8s resources
 echo "Creating Kubernetes resources..."
 kubectl apply -f k8-configmap.yaml
@@ -195,7 +195,7 @@ kubectl apply -f $K8_DEPLOYMENT_FILE
 echo "Kubernetes resources created successfully."
 echo "Geting Kubernetes resources."
 kubectl get all
-
+echo
 # Wait until the external IP is assigned to the webapp-service
 echo "Waiting for the external IP to be assigned to the webapp-service..."
 while true; do
@@ -209,7 +209,7 @@ done
 
 # Retrieve and print the external IP of the webapp-service
 WEBAPP_URL="http://$EXTERNAL_IP"
-
+echo
 echo -e "Access the CRDP Demo App at the URL below (it can take up to 60 seconds for the service to come online):"
 echo $WEBAPP_URL
 echo
@@ -217,3 +217,4 @@ echo -e "Access the CipherTrust Manager at the URL below:"
 echo $URL
 echo
 echo "Workshop launched successfully."
+echo
